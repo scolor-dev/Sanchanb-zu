5983a9d7cf839b632c7ddf08180e31a0d7306ab3
scolor-dev <221649086+scolor-dev@users.noreply.github.com>
2026-02-16 12:15:01 +0900

    feat: add routing header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
+
 import Home from "./pages/HomePage";
 import About from "./pages/AboutPage";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
+    <>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
+    </>
   );
 }
 
