6788b7a9b215859c4ad155f8ebda3d0960948641
scolor-dev <221649086+scolor-dev@users.noreply.github.com>
2026-02-18 14:17:42 +0900

    AA

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 
+async function loadText() {
+  const res = await fetch("/aa.txt");
+  const text = await res.text();
+  console.log(text);
+}
+
+loadText();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
